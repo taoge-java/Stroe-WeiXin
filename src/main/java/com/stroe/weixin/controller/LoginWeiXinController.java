9d47848784e22ec1b6d7b9878f5423dc1f01df40
zjt <zjt@DNSC01503082014>
2017-04-10 20:06:58 +0800

    微信授权登录

---
+++ src/main/java/com/stroe/weixin/controller/LoginWeiXinController.java
@@ -0,0 +1,18 @@
+package com.stroe.weixin.controller;
+
+import com.jfinal.core.Controller;
+import com.jfinal.ext.route.ControllerBind;
+
+/**
+ * 微信授权登录
+ * @author zengjintao
+ * @version 1.0
+ * 2017年4月10日 下午13:51
+ */
+@ControllerBind(controllerKey="/login")
+public class LoginWeiXinController extends Controller{
+
+	public void index(){
+		
+	}
+}
